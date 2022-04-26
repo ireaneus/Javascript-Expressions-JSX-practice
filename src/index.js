9a3105c29d42029351ab70f3686229c4d423bea9
Ireaneus <bwdave@protonmail.com>
2022-04-26 15:11:37 -0600

    Another completed

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "David L. Haines";
+const dateobj = new Date();
+const dateObject = dateobj.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {dateObject}</p>
+  </div>,
+  document.getElementById("root")
+);
